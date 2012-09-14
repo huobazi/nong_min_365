37dab887eb88a87179fd816b90e1504c45942650
huobazi <huobazi@gmail.com>
2012-09-14 14:10:15 +0800

    Impl the UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(get_create_params) 
+    if @user.save
+      redirect_to root_url, :notice => "注册成功！" 
+    else
+      render 'new'
+    end
+
+    private
+    def get_create_params
+      params[:user].slice(:username,:password)
+    end
+  end
 end
