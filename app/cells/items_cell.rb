9155b85afd6822347f097e7c9ce5a0994ab49240
Marble Wu <huobazi@gmail.com>
2012-12-29 23:36:38 +0800

    impl the homepage_area_map Changes to be committed: modified:   app/cells/items_cell.rb modified:   app/views/home/_area_map.html.erb modified:   app/views/home/index.html.erb

--- app/cells/items_cell.rb
+++ app/cells/items_cell.rb
@@ -4,9 +4,10 @@ class ItemsCell < Cell::Rails
   def homepage_latest_items(args = {})
     categories  = args[:categories]
     category_id = args[:category_id]
+    row_count = args[:row_count]
 
     @category = categories.find{|x| x.id == category_id}
-    @items = Item.latest.where(:category_id => category_id).limit(5)
+    @items = Item.latest.where(:category_id => category_id).limit(row_count)
 
     render
   end
