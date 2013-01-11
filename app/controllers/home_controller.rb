35fdea4698ec05150680267339f1d6c66333489f
Marble Wu <huobazi@gmail.com>
2013-01-11 12:02:15 +0800

    add cache for home#index" "

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
+  caches_action :index, :expires_in => 20.minutes, :layout => false
+  
   def index
     @page_tiele = '首页'
     @categories = Category.all
