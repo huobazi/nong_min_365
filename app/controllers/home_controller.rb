24909c3681a170c23a2508f1118ad34d2fdee8da
Marble Wu <huobazi@gmail.com>
2013-01-17 01:48:26 +0800

    fefactor layout of my* and search Changes to be committed: 	modified:   app/assets/stylesheets/application/_base.css.scss 	modified:   app/assets/stylesheets/front.css.scss 	modified:   app/controllers/home_controller.rb 	deleted:    app/controllers/search_controller.rb 	modified:   app/views/application/_head.html.erb 	renamed:    app/views/search/index.html.erb -> app/views/home/search.html.erb 	modified:   app/views/items/_form.html.erb 	modified:   app/views/sessions/new.html.erb 	modified:   app/views/users/change_password.html.erb 	modified:   app/views/users/new.html.erb 	modified:   config/routes.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
   caches_action :index, :expires_in => 10.minutes, :layout => false
+  caches_action :search, :expires_in => 60.minutes, :layout => false
   
   def index
     @page_tiele = '首页'
   end
+
+  def search 
+    @page_title = '产品搜索'
+    drop_breadcrumb('产品搜索', search_path)
+
+    fresh_when 
+  end
+
 end
