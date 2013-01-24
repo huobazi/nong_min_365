d0c77d2e964f399160af563e13be88c09087c30c
Marble Wu <huobazi@gmail.com>
2013-01-24 23:47:49 +0800

    impl the pages_controller_spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,4 +3,16 @@ require 'spec_helper'
 
 describe PagesController do
 
+    %w(about help contact).each do |page|
+      context 'on GET to /#{page}' do
+        before do
+        get :show, :id => page
+      end
+
+      it { should respond_with(:success) }
+      it { should render_template(page) }
+      end
+    end
+
+
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
-describe SearchController do
+#describe SearchController do
 
-end
+#end
