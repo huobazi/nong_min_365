2afebbb7b0531a647a9684f65347015c0d83f95d
Marble Wu <huobazi@gmail.com>
2013-01-27 01:10:45 +0800

    update spec

--- spec/views/admincp/site_settings/show.html.erb_spec.rb
+++ spec/views/admincp/site_settings/show.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "admincp/site_settings/show" do
   before(:each) do
-    @admincp_site_setting = assign(:admincp_site_setting, stub_model(Admincp::SiteSetting))
+    @site_setting = assign(:site_setting, stub_model(Admincp::SiteSetting))
   end
 
   it "renders attributes in <p>" do
