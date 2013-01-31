3ea28bd718432435b3f0597570a8801a84a6acdb
Marble Wu <huobazi@gmail.com>
2013-01-31 17:21:13 +0800

    fix deploy bugs

--- lib/mina/servers/production.rb
+++ lib/mina/servers/production.rb
@@ -7,13 +7,14 @@ namespace :env do
     set :sudoer,              'deployer'
     set :user,                'deployer'
     set :group,               'deployer'
-    set :rvm_path,            '/usr/local/rvm/scripts/rvm'
     set :services_path,       '/etc/init.d'          # Where your God and Unicorn service control scripts will go
     set :nginx_path,          '/etc/nginx'
     set :deploy_server,       'linodeweb01'                   # just a handy name of the server
-    set :rvm_string,          '1.9.3'
 
     invoke :defaults                                         # load rest of the config
+
+    set :rvm_path,            '/usr/local/rvm/scripts/rvm'
+    set :rvm_string,          '1.9.3'
     invoke :"rvm:use[#{rvm_string}]"
   end
 end
