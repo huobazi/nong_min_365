2ac2cc10889cebc74cd1dfdca97552c1ada076ad
Marble Wu <huobazi@gmail.com>
2013-01-31 14:31:42 +0800

    update

--- lib/mina/servers/production.rb
+++ lib/mina/servers/production.rb
@@ -11,7 +11,7 @@ namespace :env do
     set :services_path,       '/etc/init.d'          # Where your God and Unicorn service control scripts will go
     set :nginx_path,          '/etc/nginx'
     set :deploy_server,       'linodeweb01'                   # just a handy name of the server
-    set :rvm_string,          '1.9.3p374'
+    set :rvm_string,          '1.9.3'
 
     invoke :defaults                                         # load rest of the config
     invoke :"rvm:use[#{rvm_string}]"
