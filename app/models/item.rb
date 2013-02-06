b110fe4d44068baa35f97441ab30d13746ce9d4a
Marble Wu <huobazi@gmail.com>
2013-02-06 14:25:27 +0800

    add default tag_list to spider

--- app/models/item.rb
+++ app/models/item.rb
@@ -94,8 +94,8 @@ class Item < ActiveRecord::Base
   
   private
   def add_province_name_to_tags
-    if not self.tag_list.include "#{self.province_name},"
-      self.tag_list = "#{self.province_name}," + self.tag_list
+    if not self.tag_list.include? "#{self.province_name}"
+      self.tag_list = self.tag_list.push self.province_name
     end
   end
 
