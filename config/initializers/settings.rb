eecc843633ba1bb2bfacad3e2304e4edf5ced504
Marble Wu <huobazi@gmail.com>
2013-03-29 01:44:43 +0800

    site description settings

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 
-SiteSettings.save_default(:site_name, 'NongMin365')
+SiteSettings.save_default(:site_name, '天天农业')
+SiteSettings.save_default(:site_description, '专业的农产品、农资供求信息平台，覆盖全国所有村镇。')
 SiteSettings.save_default(:domain_name, 'nongmin365.com')
 SiteSettings.save_default(:admin_emails , ['huobazi@gmail.com'])
 SiteSettings.save_default(:google_account_id, 'UA-56321-6')
@@ -9,4 +10,3 @@ SiteSettings.save_default(:google_api_key, 'N/A')
 SiteSettings.save_default(:smsbao_uname, 'N/A')
 SiteSettings.save_default(:smsbao_pwd, 'N/A')
 SiteSettings.save_default(:email_sender_password, 'N/A')
-
