93ee3d6c910ce085baa9445a9d825ee73074583a
Marble Wu <huobazi@gmail.com>
2013-05-21 03:28:25 +0800

    update whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 #
-every 1.day, :at => '03:34' do
+every 1.day, :at => '03:33' do
   command 'backup perform --trigger nm365_backup --config_file config/backup.rb --data-path db --log-path log --tmp-path tmp'
 end
