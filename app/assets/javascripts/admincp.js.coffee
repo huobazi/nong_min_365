e8fe3d21b8f4679a987de53cccc300ca4e1b125f
Marble Wu <huobazi@gmail.com>
2013-07-24 16:41:04 +0800

    remove turbolinks from backend

--- app/assets/javascripts/admincp.js.coffee
+++ app/assets/javascripts/admincp.js.coffee
@@ -1,4 +1,3 @@
 //= require jquery
+//= require jquery-migrate-min
 //= require jquery_ujs
-//= require turbolinks
-
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery-migrate-min
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery.turbolinks
 
 //= require bootstrap
 //= require rails.validations
