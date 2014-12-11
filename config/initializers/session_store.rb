1217099b0dedeec40cdd4857952e156c0f8c1efd
huobazi <huobazi@gmail.com>
2014-12-11 01:59:25 +0000

    diff to rails 4.2.rc2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,4 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-NongMin365::Application.config.session_store :cookie_store, key: '_nong_min_365_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# NongMin365::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_nong_min_365_session'
