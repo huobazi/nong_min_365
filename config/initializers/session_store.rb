319bb8ff7181db77744be0ad470ae2684336898c
huobazi <huobazi@gmail.com>
2014-12-11 02:05:44 +0000

    rollback

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_nong_min_365_session'
+NongMin365.application.config.session_store :cookie_store, key: '_nong_min_365_session'
